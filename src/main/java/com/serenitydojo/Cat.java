5f033fa3246f87f8b7a26c25a044bdec024a2681
Karina Kirpicheva <karina@sayprimer.com>
2024-05-16 23:10:33 +0400

    Exercises

---
+++ src/main/java/com/serenitydojo/Cat.java
@@ -0,0 +1,26 @@
+package com.serenitydojo;
+
+public class Cat extends Pet {
+    private String name;
+    private String favoriteToy;
+
+    public Cat(String name, String favoriteToy, int age) {
+        super(name, age);
+        this.favoriteToy = favoriteToy;
+    }
+
+
+    public String getFavoriteToy() {
+        return favoriteToy;
+    }
+
+    public void setFavoriteToy(String favoriteToy) {
+        this.favoriteToy = favoriteToy;
+    }
+
+    @Override
+    public String play() {
+        return "plays with string";
+    }
+
+}
--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -2,6 +2,50 @@ package com.serenitydojo;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.categories.Categories;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class WhenCreatingObjects {
+
+    @Test
+    public void creating_a_cat() {
+        Cat spot = new Cat("Spot", "String", 5);
+
+        Assert.assertEquals(spot.getName(), "Spot");
+        Assert.assertEquals(spot.getFavoriteToy(), "String");
+        Assert.assertEquals(spot.getAge(), 5);
+    }
+
+    @Test
+    public void creating_a_dog() {
+        Dog fido = new Dog("Fido", "Bone", 5);
+
+        Assert.assertEquals(fido.getName(), "Fido");
+        Assert.assertEquals(fido.getFavoriteToy(), "Bone");
+        Assert.assertEquals(fido.getAge(), 5);
+    }
+
+    @Test
+    public void creating_a_hamster() {
+        Hamster hazel = new Hamster("Hazel", "Wheel", 1);
+
+        Assert.assertEquals(hazel.getName(), "Hazel");
+        Assert.assertEquals(hazel.getFavoriteGame(), "Wheel");
+        Assert.assertEquals(hazel.getAge(), 1);
+    }
+
+    @Test
+    public void whenAnimalsPlay() {
+        Pet fido = new Dog("Fido","Bone", 5);
+        Pet spot = new Cat("Spot","String", 5);
+        Pet hazel = new Hamster("Hazel", "Wheel", 1);
+
+        assertThat(fido.play(), equalTo("plays with bone"));
+        assertThat(spot.play(), equalTo("plays with string"));
+        assertThat(hazel.play(), equalTo("runs in wheel"));
+    }
+
 }
+
